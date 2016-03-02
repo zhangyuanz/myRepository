44ae906415c9636fc88f97ae2d38a3aef8b615fb
zhyz <zhyz@succez.com>
2016-03-02 18:13:32 +0800

    优化File2buf及测试用例

--- src/study/File2buf.java
+++ src/study/File2buf.java
@@ -10,6 +10,7 @@ package study;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 /**
  * 该类用于将制定的文件，以字节数组的形式读取
@@ -21,36 +22,32 @@ import java.io.IOException;
  */
 public class File2buf {	
 	/**
-	 * 通过从文件输入流按制定字节长度循环读取，然后写入到一个字节数组输出流中
-	 * 利用字符数组输出流toByteArray()函数将缓冲区字节转换为数组返回
+	 * 实现从把一个文件的内容写进一个字节数组。
 	 * 
 	 * @param fobj是一个文件对象参数。如果fobj不存在或者为null，则返回null
 	 * @return byte[]
 	 * @throws IOException	
 	 */
 	public byte[] file2buf(File fobj) throws IOException {
-		byte[] result = null;
 		if (fobj == null){
+			return null ;		}	
+		if (!fobj.exists()||fobj.isDirectory()){
 			return null ;
-		}	
-		FileInputStream in = new FileInputStream(fobj);
-		ByteArrayOutputStream out = new ByteArrayOutputStream(1024);//初始化制定长度，如果在写入过程中缓冲区大小不够，会自动扩容
-		result = new byte[8];
-		int len;
-		while((len=in.read(result))!=-1){
-			out.write(result, 0, len);
-			/*
-			 * write(byte[] b, int off, int len)就是将数组 b 中的 len 个字节按顺序写入输出流。
-			 * 所以如果 b 为 null，则抛出 NullPointerException。
-			 * 如果 off 为负，或 len 为负，又或者 off+len 大于数组 b 的长度，则抛出 IndexOutOfBoundsException。
-			 * 如果 len 为零，则不写入字节。否则，首先写入字节 b[off]，然后写入字节 b[off+1]，依此类推；最后一个写入字节是 b[off+len-1]。
-			 */
 		}
-		//释放资源
-		in.close();
-		out.close();
-		result = out.toByteArray();
-		return result;
+		FileInputStream in = new FileInputStream(fobj);
+		//根据文件大小创建字节数组流的大小，防止内存浪费
+		ByteArrayOutputStream out = new ByteArrayOutputStream((int)fobj.length());
+		byte[] bytes = new byte[4096];//常用4096
+		int len;
+		try{
+			while((len=in.read(bytes))!=-1){
+				out.write(bytes, 0, len);
+			}
+		}finally{
+			in.close();
+			out.close();
+		}
+		return out.toByteArray();
 	}
 	/**
 	 * Issues：在测试用例中避免所要打开的文件不存在的情况，不符合测试用例的规范原则
@@ -59,6 +56,7 @@ public class File2buf {
 	 * @param path是文件的路径
 	 * @return 返回一个文件，如果打开失败，或者文件不存在，则返回null
 	 */
+	/*
 	public File fileOpen(String path){
 		File file = new File(path);
 		if(!file.exists()){
@@ -66,4 +64,5 @@ public class File2buf {
 		}
 		return file;
 	}
+	*/
 }
--- src/study/File2bufTests.java
+++ src/study/File2bufTests.java
@@ -11,8 +11,7 @@ import static org.junit.Assert.*;
 import java.io.File;
 import java.io.IOException;
 
-import org.junit.AfterClass;
-import org.junit.Before;
+import org.junit.Assert;
 import org.junit.Test;
 /**
  * 该类专门为测试File2buf类中的file2buf方法而设计
@@ -22,9 +21,6 @@ import org.junit.Test;
  */
 public class File2bufTests{
 	private static File2buf obj = new File2buf();//首先创建一个被测试类的对象
-	
-	private static File file ;
-	private static File file1;
 	/**
 	 * 20160301-12:50
 	 * 在方法测试之前，要为file制定一个已经存在的文件，如果文件不存在，则为null
@@ -39,37 +35,57 @@ public class File2bufTests{
 	 * <pre>
 	 * @throws Exception
 	 */
-	@Before
-	public void setUp() throws Exception {
-		file = obj.fileOpen("D:/test.txt");
-		file1 = obj.fileOpen("D:/test1.txt");
-	}
 	/**
 	 * 20160301-11:40
 	 * 测试file2buf方法，定义array接受函数返回值，并断言是否是期望的结果
 	 * 20160301-14:25
-	 * 用array测试正常输入的结果，用array1测试异常：当文件不存在时的结果
+	 * 用array测试正常输入的结果，用array1测试异常：当文件不存在时的结果,arrray2测只是一个路径的情况
 	 * @throws IOException
 	 */
 	@Test
 	public void testfile2buf() throws IOException {
-		byte[] array = obj.file2buf(file);
+		byte[] array = obj.file2buf(new File("d:/test.txt"));
 		String text = "﻿这是一个测试用的文本，是为了测试java基础练习题一的file2buf函数的实现而存在的。";
-		assertTrue(text.equals(new String(array)));
-		byte[] array1 = obj.file2buf(file1);
-		assertTrue(array1==null);
+		Assert.assertEquals(text, new String(array));
 	}
-	
+	/**
+	 * 测试文件不存在
+	 * @throws IOException
+	 */
+	@Test
+	public void testNotExist() throws IOException{
+		byte[] array = obj.file2buf(new File("d:/test1.txt"));
+		assertEquals(array,null);
+	}
+	/**
+	 * 测试文件输入只是一个路径
+	 * @throws IOException
+	 */
+	@Test 
+	public void testIsDir() throws IOException{
+		byte[] array = obj.file2buf(new File("d:/"));
+		assertEquals(array,null);
+	}
+	/**
+	 * 测试文件输入为空
+	 * @throws IOException
+	 */
+	@Test 
+	public void testNull() throws IOException{
+		byte[] array = obj.file2buf(null);
+		assertEquals(array,null);
+	}
 	/**
 	 * 释放测试用例所占用的资源
 	 * 20160301-14:30
 	 * 去掉@After中file与file1的释放，放在AfterClass中，因为本例只有一个函数测试，After与AfterClass没有必要分开写
 	 * 但是当有很多个测试函数时，每次在After中关闭资源会比较浪费时间，都放在AfterClass中可以提高运行效率
 	 */
+	/*
 	@AfterClass
 	public static void tearDownAfterClass() throws Exception {
 		obj = null;
 		file = null;
 		file1 = null;
-	}
+	}*/
 }
