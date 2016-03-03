ea0c0257ec609660f0a13d170d306fb1cafc45ed
zhyz <zhyz@succez.com>
2016-03-03 10:55:43 +0800

    int2Hex优化完毕

--- src/study/To16H.java
+++ src/study/To16H.java
@@ -10,50 +10,44 @@ package study;
  */
 public class To16H {
 	/**
-	 * 通过定义一个数组来存储参数a对16循环取得的余数
-	 * 然后查找ASCII码表转换成字符，构造出字符串并返回
-	 * 
-	 * @param	a是一个无符号整数
-	 * @return	如果a是负数，则返回提示信息，否则返回对应的十六进制字符串 			
+	 * 将一个整数转换成字符串，如果输入的整数不合法，则抛出异常，否则返回该整数对应的字符串
+	 * <pre>
+	 * eg:"0x0H"==0;"0xAH"==10;"-0xAH"==-10;
+	 * </pre>
+	 * @param	a 一个32有符号整数
+	 * @return  str 是转换后得到的字符串 			
 	 */
 	public  String int2Hex(int a){
-		if(a<0){
-			return "不能输入负数！";
-		}
-		/*
-		 * 20160301-15：13
-		 * Issues:在测试时发现其他用例正常，当a为0时，结果是0xH，不符合要求
-		 * 因此添加此处判断预处理
-		 */
-		if(a==0){
-			return "0x0H";
-		}
-		String hex = null;
-		int n=0;
-		char[] b = new char[8];
-		while(a!=0){
-			int x = a%16;//b是一个临时存放a余数的数组，初始化长度为8，有可能产生越界，需要根据a的最大可能值确定一个合理的size
-			b[n] = (x<10)?(char)(x+48):(char)(x+55);
-			a=a/16;
-			n++;
-		}
-		//反序
-		for(int i = 0;i < n/2;i++){
-			char temp = b[i];
-			b[i] = b[n-1-i];
-			b[n-1-i] = temp;
-		}
-		/*char[] c = new char[n];//根据实际的长度初始化c，不浪费内存
-		for(int i=0;i<n;i++){
-			if(b[i]<10){
-				c[n-i-1]=(char)(b[i]+48);
-			}else{
-				c[n-i-1]=(char)(b[i]+55);
+		String str = null;
+		//java整数范围检查
+		if(a < -2147483648||a > 2147483647){
+			throw new IllegalArgumentException("整数越界");
+			//throw 抛出异常？
+		}else{
+			if(a==0){
+				return "0x0H";
 			}
-		}*/
-		hex = new String(b);
-		hex = "0x"+hex+"H";
-		return hex;
-		
+			
+			StringBuilder hex = new StringBuilder();
+			if(a < 0){//负数处理
+				a = a*(-1);
+				while(a!=0){
+					int x = a%16;
+					char c = (x<10)?(char)(x+48):(char)(x+55);
+					hex.append(c);
+					a=a/16;
+				}
+				str = "-0x"+hex.reverse().toString()+'H';
+			}else{//正数处理
+				while(a!=0){
+					int x = a%16;
+					char c = (x<10)?(char)(x+48):(char)(x+55);
+					hex.append(c);
+					a=a/16;
+				}
+				str = "0x"+hex.reverse().toString()+'H';
+			}
+		}
+		return str;
 	}
 }
--- src/study/To16HTests.java
+++ src/study/To16HTests.java
@@ -5,45 +5,41 @@
  */
 package study;
 
-import org.junit.AfterClass;
 import org.junit.Assert;
 import org.junit.Test;
 /**
- * 该类是测试类，为测试int2Hex而存在
- * 拥有tearDownAfterClass，用于资源回收，testint2Hex测试int2Hex的运行结果
- * 一共给出负数，0，和正整数三类测试
- * 
+ * 测试用例
  * @author zhyz
  *
  */
 public class To16HTests {
 	private static To16H obj = new To16H();
 	/**
-	 * 测试结束时释放资源
-	 * @throws Exception
-	 */
-	@AfterClass
-	public static void tearDownAfterClass() throws Exception {
-		obj = null;
-	}
-	/**
-	 * 测试intHex方法，通过断言给定不同的输入，看是否得到期望的结果
-	 * 负数提示不能输入负数，0则是0x0H，整数就对应的十六进制字符串相等
+	 * 测试intHex方法的一般正常输入
 	 */
 	@Test
 	public void testint2Hex() {
-		Assert.assertTrue("不能输入负数！".equals(obj.int2Hex(-1)));
-		Assert.assertTrue("0x0H".equals(obj.int2Hex(0)));
-		Assert.assertTrue("0x1H".equals(obj.int2Hex(1)));
-		Assert.assertTrue("0xAH".equals(obj.int2Hex(10)));
-		Assert.assertTrue("0xBH".equals(obj.int2Hex(11)));
-		Assert.assertTrue("0xFH".equals(obj.int2Hex(15)));
-		Assert.assertTrue("0x10H".equals(obj.int2Hex(16)));
-		Assert.assertTrue("0x11H".equals(obj.int2Hex(17)));
-		Assert.assertTrue("0x64H".equals(obj.int2Hex(100)));
-		Assert.assertTrue("0xC8H".equals(obj.int2Hex(200)));
-		Assert.assertTrue("0xFFFFH".equals(obj.int2Hex(65535)));
-		Assert.assertTrue("0x1869FH".equals(obj.int2Hex(99999)));
+		test("-0x1H",-1);
+		test("0x0H",0);
+		test("0x1H",1);
+		test("0xAH",10);
+		test("-0xAH",-10);
+		test("0xBH",11);
+		test("0xFH",15);
+		test("0x10H",16);
+		test("0x64H",100);
+		test("0xFFFFH",65535);
 	}
-
+	/**
+	 * 测试intHex方法参数的边界值
+	 */
+	@Test
+	public void testBoundary(){
+		test("0x7FFFFFFFH",2147483647);
+		test("-0x(0000000H",-2147483648);
+		
+	}
+	public void test(String exp,int ori){
+		Assert.assertEquals(exp,obj.int2Hex(ori));
+	}
 }
