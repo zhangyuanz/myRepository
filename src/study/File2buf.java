e249c7abaed878fa9b2691257717ee3fa18579ec
zhyz <zhyz@succez.com>
2016-03-07 10:50:01 +0800

    文件长度超过Integer.MAX_VALUE抛出不支持异常 修改为超过2*Integer.MAX_VALUE+1

--- src/study/File2buf.java
+++ src/study/File2buf.java
@@ -13,30 +13,31 @@ import java.io.FileInputStream;
 import java.io.IOException;
 
 /**
- * 该类用于将制定的文件，以字节数组的形式读取
- * 拥有唯一file2buf方法，返回制定文件的字符数组
- * @author	zhyz
- * @creatdate	20160301
+ * 该类用于将制定的文件，以字节数组的形式读取 拥有唯一file2buf方法，返回制定文件的字符数组
+ * 
+ * @author zhyz
+ * @creatdate 20160301
  */
 public class File2buf {
 	/**
 	 * 实现从把一个文件的内容写进一个字节数组。如果不存在或者为null，或者只是一个路径，则返回null
 	 * 
-	 * @param fobj 是一个文件对象参数。
+	 * @param fobj
+	 *            是一个文件对象参数。
 	 * @return byte[] 是一位字节数组。
-	 * @throws IOException	
+	 * @throws IOException
 	 */
 	public byte[] file2buf(File fobj) throws IOException {
 		// 空判断要在最开始？
 		if (fobj == null) {
 			return null;
 		}
-		//不存在或者只是一个路径，直接返回null
+		// 不存在或者只是一个路径，直接返回null
 		if (!fobj.exists() || fobj.isDirectory()) {
 			return null;
 		}
-		if (fobj.length() > Integer.MAX_VALUE) {
-			throw new IllegalArgumentException("不支持大小超过2G");
+		if (fobj.length() > 2 * Integer.MAX_VALUE + 1) {
+			throw new IllegalArgumentException("不支持大小超过2G文件");
 		} else {
 			FileInputStream in = null;
 			ByteArrayOutputStream out = null;
